36117ae7a9e19a961643bf3ecdc2e7f2353670bb
shubham007x <shubham.agdari@gmail.com>
2023-09-12 18:46:14 +0530

    Template

--- src/Redux/Action.js
+++ src/Redux/Action.js
@@ -1,5 +1,19 @@
 import axios from "axios";
-
+const BaseURL = `http://localhost:8080/products`;
 const createAction = (type, payload) => {
   return { type, payload };
 };
+
+// example function for thunks
+// export const getRestaurants = (payload) => {
+//   return async function (dispatch) {
+//     dispatch(createAction(GET_RESTAURANTS_REQUEST));
+//     try {
+//       const res = await axios.get(BaseURL);
+//       dispatch(createAction(GET_RESTAURANTS_SUCCESS, res.data));
+//       console.log(res.data);
+//     } catch (error) {
+//       dispatch(createAction(GET_RESTAURANTS_FAILURE));
+//     }
+//   };
+// };
